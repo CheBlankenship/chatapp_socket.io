a1a3eb631c3e7508f29e711449b20911e4afa47c
Che B <cnakamura336@gmail.com>
2016-12-08 13:15:27 -0500

    update

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express'
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+app.get('/', function(req,res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log("listening on *3000");
+});
